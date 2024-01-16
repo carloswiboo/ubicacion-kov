968d1931c9b5fa01c27ce0b5e22c24186c102b4a
Carlos Francisco Estrada Garcidueñas <cestradagar@guanajuato.gob.mx>
2024-01-16 11:31:12 -0600

    Ajustes que no sirven

--- components/MapaComponent/MapaComponent.js
+++ components/MapaComponent/MapaComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Circle,
   CircleMarker,
@@ -20,9 +20,49 @@ const rectangle = [
 ];
 
 const MapaComponent = () => {
-    
+  const [posiciones, setPosiciones] = React.useState([
+    {
+      id: 1,
+      color: "red",
+      bound: [
+        [50, -50],
+        [55, -45],
+      ],
+      posicions: [[40.51, 50]],
+    },
+    {
+      id: 2,
+      color: "black",
+      bound: [
+        [40, -50],
+        [45, -45],
+      ],
+      posicions: [[51.51, -0.12]],
+    },
+  ]);
+
+  const rectangleRefs = posiciones.map(() => useRef());
+
+  const handleRectangleDrag = (index, event) => {
+    const latlng = event.target.getLatLng();
+    const newPositions = [...posiciones];
+    newPositions[index].posicions = [[latlng.lat, latlng.lng]];
+    setPosiciones(newPositions);
+  };
+
+  React.useEffect(() => {
+    rectangleRefs.forEach((ref, index) => {
+      const rectangle = ref.current.leafletElement;
+
+      rectangle.dragging.enable();
+
+      rectangle.on("dragend", (event) => handleRectangleDrag(index, event));
+    });
+  }, [rectangleRefs]);
+
   return (
     <>
+      {JSON.stringify(posiciones)}
       <MapContainer
         style={{ height: "100vh" }}
         center={[0, 0]}
@@ -37,14 +77,21 @@ const MapaComponent = () => {
         />
 
         <LayerGroup>
-          <Rectangle
-            bounds={rectangle}
-            center={[51.51, -0.12]}
-            pathOptions={{ color: "red" }}
-          >
-            <Tooltip>Tooltip for CircleMarker</Tooltip>
-            <text>Carlos</text>
-          </Rectangle>
+          {posiciones.map((posicion, index) => {
+            return (
+              <Rectangle
+                key={posicion.id}
+                bounds={posicion.bound}
+                center={posicion.posicions}
+                ref={rectangleRefs[index]}
+                pathOptions={{ color: posicion.color }}
+                draggable={true}
+                onDrag={(event) => handleRectangleDrag(index, event)}
+              >
+                <Tooltip>Tooltip for CircleMarker</Tooltip>
+              </Rectangle>
+            );
+          })}
         </LayerGroup>
       </MapContainer>
     </>
